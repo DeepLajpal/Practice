dbd300c69d0a05addc0720856d0a241b7e7df000
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-12-02 18:25:47 +0530

    Node JS Practice - Setup:  initialize project with package.json, package-lock.json, and .gitignore; add app.js for user greeting

---
+++ Node JS Practice/app.js
@@ -0,0 +1,8 @@
+// Practice
+
+// console.log("My first node.js application!");
+// console.log(process.argv);
+
+// Practice Exercise
+const userName = process.argv[2];
+console.log(`Hello ${userName || "User"}! Welcome to the Node.js`);
