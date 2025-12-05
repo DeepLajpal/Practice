78ab66190ecea9286bd8ce803bb6a52b4d10965a
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-12-05 18:02:57 +0530

    Node.js: Practice append and delete file operations in Node.js

--- NodeJS_Practice/core2/1.js
+++ NodeJS_Practice/core2/1.js
@@ -1,6 +1,7 @@
-// reading file
 const fs = require("fs");
 const util = require("util");
+
+// reading file
 
 // A) Asynchronous args(file path, charactor encoding from hexa byte to human readable, callback - runs when the file reading finishes., )
 // fs.readFile("input.txt", "utf8", (err, data) => {
@@ -34,3 +35,21 @@ const util = require("util");
 
 // B) Sync
 // fs.writeFileSync("output.txt", "hello world");
+
+// Appending files
+// A) Async
+// fs.appendFile("output.txt", "\nHeyIamAppended", (err) => {
+//   if (err) return console.error(err);
+// });
+
+// B) Sync
+// fs.appendFileSync("output.txt", "\nHeyIamAppended2\n");
+
+// Deleting files
+// A) Async
+// fs.unlink("output.txt", (err) => {
+//   if (err) return console.error(err);
+// });
+
+// B) Sync
+fs.unlinkSync("output.txt");
