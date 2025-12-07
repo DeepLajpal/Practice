7c8d44e8f2c29df65348a663f09c31ae3b6c53ab
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-12-07 19:10:51 +0530

    Refactor mini server project: Simplify request handling by storing URL in a variable and streamline product response structure

--- NodeJS_Practice/core3/mini-server-project.js
+++ NodeJS_Practice/core3/mini-server-project.js
@@ -1,26 +1,26 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  if (req.url == ("/" || "")) {
+  const url = req?.url;
+  if (url == ("/" || "")) {
     res.setHeader("Content-Type", "text/html");
     res.end("<div>Home Page</div>");
-  } else if (req.url == "/about") {
+  } else if (url == "/about") {
     res.setHeader("Content-Type", "text/plain");
     res.end("Hi Iam about page");
-  } else if (req.url == "/products") {
+  } else if (url == "/products") {
+    const products = [
+      {
+        id: 1,
+        name: "Apple",
+      },
+      {
+        id: 2,
+        name: "Grapes",
+      },
+    ];
     res.setHeader("Content-Type", "application/json");
-    res.end(
-      JSON.stringify([
-        {
-          id: 1,
-          name: "Apple",
-        },
-        {
-          id: 2,
-          name: "Grapes",
-        },
-      ])
-    );
+    res.end(JSON.stringify(products));
   } else {
     res.statusCode = 404;
     res.end("Page Not Found By Deep!");
