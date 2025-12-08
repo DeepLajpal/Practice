51fa625ce57087b5dbe9a054139b97ca917f99f2
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2025-12-08 17:52:20 +0530

    Node.js: working with core4 Express.js Basics practice explanations

---
+++ NodeJS_Practice/core4/index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+
+const app = express();
+
+// 1. Routing
+
+app.get("/", (req, res) => {
+  res.statusCode = 404;
+  res.send("Page not found @deep");
+});
+app.get("/home", (req, res) => {
+  res.send("Thank you for visiting /home route");
+});
+app.post("/login", (req, res) => {
+  res.send("Thank you for visiting /login route");
+});
+app.put("/user/1", (req, res) => {
+  res.send("Thank you for visiting /user put route");
+});
+app.delete("/user/1", (req, res) => {
+  res.send("Thank you for visiting /user delete route");
+});
+
+// 3. Params
+app.get("/products/search", (req, res) => {
+  try {
+    res.send(
+      `Hey your query params are: \n Keyword: ${req.query.keyword}\n Page: ${req.query.page} `
+    );
+  } catch (err) {
+    res.send(`Unable to resolve ${req.path}`);
+  }
+});
+
+//2. Route Parameters
+
+app.get("/products/:id", (req, res) => {
+  console.log("iam route /products/:id");
+  res.send(
+    `Thank you for visiting route ${req.path} and params ${req.params.id}`
+  );
+});
+
+// 4. Middleware
+
+app.listen(3000, () => console.log("Server running on port 3000"));
