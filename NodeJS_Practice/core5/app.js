fe7f4af6297fc93fb87ded25b6e821a99c845e75
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2026-01-01 09:37:14 +0530

    refactor: Practice Node.js, standardize response objects for task API endpoints and also created endpoint new put (update) api for one task a time

--- NodeJS_Practice/core5/app.js
+++ NodeJS_Practice/core5/app.js
@@ -3,9 +3,13 @@ import pool from "./db2.js";
 
 const app = express();
 const port = 3000;
-const resObj = {
+const successResObj = {
   success: true,
   message: "Request Success!",
+};
+const errObjRes = {
+  success: false,
+  message: "Request Failed! Please try again Later.",
 };
 
 app.use(express.json());
@@ -14,32 +18,46 @@ app.get("/tasks", async (req, res) => {
   try {
     const result = await pool.query("SELECT * from tasks");
 
-    res.json({ ...resObj, data: result.rows });
+    res.json({ ...successResObj, data: result.rows });
   } catch (error) {
-    const resObj = {
-      success: false,
-      message: "Failed to get the data, please try again later!",
-    };
-    res.status(500).json(resObj);
+    res.status(500).json(errObjRes);
     console.log(error.message);
   }
 });
 app.post("/tasks", async (req, res) => {
   try {
     const { title } = req.body;
+    // using parameterized queries for protection against SQL injection type attacks
     const result = await pool.query(
       "INSERT INTO tasks (title, completed) VALUES ($1, $2) RETURNING *",
       [title, false]
     );
     res.status(201).json({
-      ...resObj,
+      ...successResObj,
       message: "Task Inserted Succsss!",
       data: result.rows[0],
     });
   } catch (error) {
-    res
-      .status(500)
-      .json({ ...resObj, success: false, message: "failed to post data" });
+    res.status(500).json(errObjRes);
+    console.log("Error: ", error.message);
+  }
+});
+
+app.put("/tasks/:id", async (req, res) => {
+  const { title, completed } = req.body;
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query(
+      "UPDATE tasks SET title = $2, completed = $3 WHERE id = $1 RETURNING *",
+      [id, title, completed]
+    );
+    if (result.rows.length == 0) {
+      return res.status(404).send({ ...errObjRes, message: "id not found" });
+    }
+    res.status(201).send({ ...successResObj, data: result.rows });
+  } catch (error) {
+    res.status(500).send(errObjRes);
     console.log("Error: ", error.message);
   }
 });
