195c32d2f9b63bf4d3bea1ef982ab127a9f5bb90
Deep Navin Lajpal <115221643+DeepLajpal@users.noreply.github.com>
2026-01-07 22:20:41 +0530

    refactor: move server initialization to a separate server.js file and export app from app.js

--- NodeJS_Practice/core5/app.js
+++ NodeJS_Practice/core5/app.js
@@ -1,11 +1,8 @@
 import express from "express";
 import taskRouter from "./routes/task.routes.js";
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 app.use("/", taskRouter);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+export default app;
